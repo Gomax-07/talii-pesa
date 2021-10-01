4684f09cd3a5b2ba7692cebca11d6798344a08f7
go-max07 <johnmetry17@gmail.com>
2021-10-01 05:49:55 +0300

    Added new urls to house model

--- rent/urls.py
+++ rent/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path, include
 from . import views
+from .views import *
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('', views.landing, name='landing'),
+
+# House_paths
     path('house/', views.HouseListView.as_view(), name='house'),
-    path('house/<int:pk>', views.HouseDetailView.as_view(), name='house-detail'),
-    path('', views.landing, name='landing')
+    path('house/<int:pk>/', views.HouseDetailView.as_view(), name='house-detail'),
+    path('house/create/', house_create, name='house_create'),
+
+
+#Tenants_paths
+    
 ]
 
