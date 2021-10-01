722ac1459bfd20312a01094b46f68e0e70f31f69
go-max07 <johnmetry17@gmail.com>
2021-10-01 05:23:22 +0300

    Added form models

---
+++ rent/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from .models import *
+
+# House forms
+
+class HouseModelForm(forms.ModelForm):
+    class Meta:
+        model = House
+        fields = (
+            'house_name',
+            'tenant',
+            'summary',
+            'caretaker',
+        )
+
+
+class HouseForm(forms.Form):
+    house_name = forms.CharField()
+    tenant = forms.CharField()
+    summary = forms.CharField()
+    Caretaker = forms.CharField()
+
+
+# Caretaker form
+
+class CaretakerModelForm(forms.ModelForm):
+    class Meta:
+        model = Caretaker
+        fields = (
+            'name',
+        )
+
+
+class CaretakerForm(forms.Form):
+    name = forms.CharField()
+
+# Rent forms
+
+class RentModelForm(forms.ModelForm):
+    class Meta:
+        model = Rent
+        fields = (
+            'house',
+            'amount',
+            'payment_date',
+            'status',
+        )
+
+
+class RentForm(forms.Form):
+    house = forms.CharField()
+    amount = forms.IntegerField()
+    payment_date = forms.DateField()
+    status = forms.CharField()
